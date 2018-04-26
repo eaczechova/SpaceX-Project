fce0235a0decb59b6fec68b1495b6a51e4153dcf
Ewa Czech <eaprojekty@gmail.com>
2018-04-27 01:08:27 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { hot } from 'react-hot-loader';
+import * as React from 'react';
+import LaunchDetails from './components/LaunchDetails';
+
+import './styles/theme.sass';
+
+class App extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <main>
+        <LaunchDetails />
+      </main>
+    );
+  }
+}
+
+export default hot(module)(App);
---
+++ src/components/LaunchDetails.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+
+class LaunchDetails extends React.Component {
+  render() {
+    return <h1>Hello</h1>;
+  }
+}
+
+export default LaunchDetails;
